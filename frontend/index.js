2cecc02b34fea85b5e78de013236855e8b89cb3a
jonas <jonas.ralin@gmail.com>
2024-05-14 14:36:37 +0200

    generate login dom function, get login info function

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,45 @@
-console.log("hello world");
+const loginSection = document.getElementById("loginSection");
+const publicSection = document.getElementById("publicSection");
+const userSection = document.getElementById("userSection");
+
+
+
+const generateLoginDOM = () => {
+    //Login Article
+    const article = document.createElement("article");
+    article.setAttribute("id", "login");
+
+    //Username
+    const usernameInput = document.createElement("input");
+    usernameInput.setAttribute("type", "text");
+    usernameInput.setAttribute("id", "login_username");
+    usernameInput.setAttribute("placeholder", "Username");
+
+    //Password
+    const passwordInput = document.createElement("input");
+    passwordInput.setAttribute("type", "password");
+    passwordInput.setAttribute("id", "login_password");
+    passwordInput.setAttribute("placeholder", "Password");
+
+    //Login Btn
+    const loginBtn = document.createElement("button");
+    loginBtn.setAttribute("type", "button");
+    loginBtn.setAttribute("id", "login_btn");
+    loginBtn.textContent = "Log In";
+
+    //Append Elements
+    article.appendChild(usernameInput);
+    article.appendChild(passwordInput);
+    article.appendChild(loginBtn);
+
+    loginSection.appendChild(article);
+}
+
+generateLoginDOM();
+
+const getLoginInput = () =>{
+    const username = document.getElementById("login_username").value;
+    const password = document.getElementById("login_password").value;
+
+    return [username, password];
+}
