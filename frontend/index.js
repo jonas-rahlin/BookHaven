fb73ca02d6481f67352c5c7b78ac0b5aaf6f1dcd
jonas <jonas.ralin@gmail.com>
2024-05-14 15:35:41 +0200

    registerLoginDOM generation

--- frontend/index.js
+++ frontend/index.js
@@ -3,13 +3,9 @@ const publicSection = document.getElementById("publicSection");
 const userSection = document.getElementById("userSection");
 
 
-//Login Section
-const getLoginInput = () =>{
-    const username = document.getElementById("login_username").value;
-    const password = document.getElementById("login_password").value;
-    return {username, password};
-}
+/* Login Section */
 
+//Login
 const generateLoginDOM = () => {
     //Login Article
     const article = document.createElement("article");
@@ -37,12 +33,27 @@ const generateLoginDOM = () => {
         login();
     });
 
+    const registerBtn = document.createElement("button");
+    registerBtn.setAttribute("type", "button");
+    registerBtn.setAttribute("id", "register_btn");
+    registerBtn.textContent = "Create Account";
+    registerBtn.addEventListener("click", ()=> {
+
+    });
+
     //Append Elements
     article.appendChild(usernameInput);
     article.appendChild(passwordInput);
     article.appendChild(loginBtn);
+    article.appendChild(registerBtn);
 
     loginSection.appendChild(article);
+}
+
+const getLoginInput = () =>{
+    const username = document.getElementById("login_username").value;
+    const password = document.getElementById("login_password").value;
+    return {username, password};
 }
 
 const login = async () => {
@@ -68,13 +79,52 @@ const login = async () => {
     }
 }
 
-const activeUser = () =>{
-    
-    return {id, username, jwt};
+//Register
+const registerLoginDOM = () => {
+    //Register Article
+    const article = document.createElement("article");
+    article.setAttribute("id", "register");
+
+    //Username
+    const regUsernameInput = document.createElement("input");
+    regUsernameInput.setAttribute("type", "text");
+    regUsernameInput.setAttribute("id", "register_username");
+    regUsernameInput.setAttribute("placeholder", "Username");
+
+    //Email
+    const regEmailInput = document.createElement("input");
+    regEmailInput.setAttribute("type", "email");
+    regEmailInput.setAttribute("id", "register_email");
+    regEmailInput.setAttribute("placeholder", "Email");
+
+    //Password
+    const regPasswordInput = document.createElement("input");
+    regPasswordInput.setAttribute("type", "password");
+    regPasswordInput.setAttribute("id", "register_password");
+    regPasswordInput.setAttribute("placeholder", "Password");
+
+    //Login Btn
+    const submitUserBtn = document.createElement("button");
+    submitUserBtn.setAttribute("type", "button");
+    submitUserBtn.setAttribute("id", "submitUser_btn");
+    submitUserBtn.textContent = "Join";
+    submitUserBtn.addEventListener("click", ()=> {
+        console.log("working...");
+        register();
+    });
+
+    //Append Elements
+    article.appendChild(regUsernameInput);
+    article.appendChild(regPasswordInput);
+    article.appendChild(regEmailInput);
+    article.appendChild(submitUserBtn);
+
+    loginSection.appendChild(article);
 }
 
-const loginBtn = document.getElementById("");
-
+const register = async () =>{
+    
+}
 
 
 
@@ -90,7 +140,7 @@ const loginBtn = document.getElementById("");
 
 
 generateLoginDOM();
-
+registerLoginDOM();
 
 
 /* const register = async () => {
